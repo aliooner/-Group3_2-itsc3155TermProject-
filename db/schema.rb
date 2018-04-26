4601cda6707d63f5254e2803dd5b71f1cefa00e6
Muhammed Oner <alioooner@gmail.com>
2018-04-26 21:22:41 +0000

    Add a login model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402170027) do
+ActiveRecord::Schema.define(version: 20180426212153) do
+
+  create_table "logins", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sales", force: :cascade do |t|
     t.string "Username"
