c67fd3877e2de54b7079ea43f60603f2569d8335
Aaron Yow <ayow2@uncc.edu>
2018-04-29 15:42:56 +0000

    finalized google authenticate on back-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failre', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
+  
+  resource :sessions, only: [:create, :destroy]
+
   get 'welcome/index'
   
   resources :sales
