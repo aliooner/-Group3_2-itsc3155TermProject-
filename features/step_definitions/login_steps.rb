0d84d51e8ef8f01dc2ca0a36ee00fb320621b156
Muhammed Oner <alioooner@gmail.com>
2018-04-30 14:56:46 +0000

    Set up item feature scenario 4

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -5,3 +5,7 @@ end
 When("I click on the {string} button") do |page_name|
   click_button page_name # Write code here that turns the phrase above into concrete actions
 end
+
+Then("I should see {string} link") do |page_name|
+  click_link page_name # Write code here that turns the phrase above into concrete actions
+end
